4ac4eba5d6bde745633905aa0175beb5ad33d3b3
Kris Frye <kfrye@pdx.edu>
2015-02-25 17:13:12 -0800

    Added initial measurements

--- project/script.sh
+++ project/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
 for k in heapsort mergesort quicksort dual_pivot_qs; do
-  for j in 1T 5T 10T 50T 100T 500T 1M 5M 10M 50M; do
-    for i in `seq 1 2`; do 
+  for j in 1T 5T 10T 50T 100T 500T 1M 5M 10M; do
+    for i in `seq 1 100`; do 
       java -cp $k $k $j $k/$j.out
     done
   done
